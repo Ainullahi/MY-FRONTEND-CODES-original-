61f088d2d8d13b28d16293eb65b86c867f541810
unknown <ainullahisulaiman@gmail.com>
2026-02-11 16:17:01 -0800

    initial commit

---
+++ week5/arithimetic.js
@@ -0,0 +1,17 @@
+// addition of num
+let a = 5;
+let b = 5;
+let answer = a + b;
+console.log(a , '+', b, '=', answer)
+
+// subtraction of num
+let d = 12;
+let e = 2;
+let ans = d + e;
+console.log( ans)
+
+// product of num
+let g = 3;
+let h = 7;
+let result = g * h;
+console.log(result)
---
+++ week5/intro.js
@@ -0,0 +1,14 @@
+// jvs intro comments
+console.log('first topic')
+console.log('this is 2026')
+console.log('and i have started java script')
+console.log('it is going great')
+console.log('but i have been making mistakes')
+console.log('but with practise')
+console.log('I WILL DO BETTER ')
+
+// single line comment 
+
+/*
+
+*/
